ccebb460fe1fac68a18a3b6adba00aab98233c3f
xiaofan <1353815633@qq.com>
2019-06-29 00:15:32 +0800

    create HomeHeader

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import fastClick from 'fastclick'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body) // 解决手机端点击有 300 毫秒延迟的问题
--- src/pages/Home/components/Header.vue
+++ src/pages/Home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">left</div>
-    <div class="header-input">input</div>
-    <div class="header-right">right</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe667;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe615;</span>
+      输入城市景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
@@ -13,5 +21,34 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import '../../../assets/styles/varibles.styl'
+  .header
+    display flex
+    line-height .86rem
+    background $bgColor
+    color #fff
+    .header-left
+      width .64rem
+      float left
+      .back-icon
+        text-align center
+        font-size .4rem
+    .header-input
+      flex 1
+      background #fff
+      color #ccc
+      border-radius .1rem
+      margin-top .12rem
+      height .64rem
+      line-height .64rem
+      margin-left .2rem
+      padding-left .2rem
+    .header-right
+      width 1.24rem
+      float right
+      text-align center
+      .arrow-icon
+        font-size .24rem
+        margin-left -.04rem
 
 </style>
