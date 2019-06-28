98aa2ba50181a3afe437d681e26568ad33eeb6e8
xiaofan <1353815633@qq.com>
2019-06-28 11:19:31 +0800

    init travel

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import fastClick from 'fastclick';
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)   // 解决手机端点击有 300 毫秒延迟的问题
+
 
 new Vue({
   router,
