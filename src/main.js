d064cfdbcef9e35fdc0b0cbecc7c9e2f90abf832
xiaofan <1353815633@qq.com>
2019-06-29 22:57:20 +0800

    create homeswiper

--- src/main.js
+++ src/main.js
@@ -2,16 +2,16 @@
  * @Author: DevZhang
  * @Date: 2019-06-29 13:19:03
  * @Last Modified by: DevZhang
- * @Last Modified time: 2019-06-29 13:19:38
+ * @Last Modified time: 2019-06-29 22:20:32
  */
 
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import fastClick from 'fastclick'
-import 'assets/styles/reset.css'
-import 'assets/styles/border.css'
-import 'assets/styles/iconfont.css'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body) // 解决手机端点击有 300 毫秒延迟的问题
