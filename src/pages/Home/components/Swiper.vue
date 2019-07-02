165715d6d67bb603dcfb89271f9067daaa7c5915
xiaofan <1353815633@qq.com>
2019-07-02 13:08:23 +0800

    begin create home-recommend

--- src/pages/Home/components/Swiper.vue
+++ src/pages/Home/components/Swiper.vue
@@ -15,7 +15,8 @@ export default {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
-        loop: true
+        loop: true,
+        autoplay : 2000
       },
       imgUrlList: [
         {
@@ -25,6 +26,14 @@ export default {
         {
           id: '0002',
           imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/87a224d0349d94a11e97f31aa1aba4f5.jpg_750x200_1f78af87.jpg'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/1f69b3ab28f9962158c798a8a3f5b4b3.jpg_750x200_fb4bf7c4.jpg'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/8254351bdfba7389f5a4de4416b9d4f4.png_750x200_843e98ad.png'
         }
       ]
     }
